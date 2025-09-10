316dab246da9dd2eeb293142c1f2f4acf9eddff4
DanielSln <12danielsolano@gmail.com>
2025-09-10 16:27:04 -0300

    ionbutton

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,13 +5,14 @@ import {
   IonTitle,
   IonContent,
   IonInfiniteScroll,
+  IonButton,
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInfiniteScroll],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInfiniteScroll, IonButton ],
 })
 export class HomePage {
   constructor() {}
