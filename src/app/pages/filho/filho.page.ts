d0b00558ee292c7116e495612af74753e4e2d1dc
DanielSln <12danielsolano@gmail.com>
2025-09-18 16:17:45 -0300

    menu on page son

--- src/app/pages/filho/filho.page.ts
+++ src/app/pages/filho/filho.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenuButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-filho',
   templateUrl: './filho.page.html',
   styleUrls: ['./filho.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonButtons]
 })
 export class FilhoPage implements OnInit {
 
