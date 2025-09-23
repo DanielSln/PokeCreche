3af8ffd70aa9f2c67d7b429154ed827b27db4fe0
DanielSln <12danielsolano@gmail.com>
2025-09-23 17:14:03 -0300

    criando a tela de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,20 @@ import {
   IonAvatar,
   IonLabel,
   IonText,
+  IonIcon,
 } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {
+  person,
+  calendar,
+  notifications,
+  happy,
+  chatbubbles,
+  menu,
+  menuOutline,
+  menuSharp,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -30,13 +43,50 @@ import {
     IonAvatar,
     IonLabel,
     IonText,
+    IonIcon,
   ],
 })
 export class AppComponent {
   profile = {
     name: 'Daniel',
-    email: 'daniel7'
+    email: 'daniel7',
   };
-  
-  constructor() {}
+
+  pages = [
+    { title: 'Menu', 
+    url: '/folder/Menu', 
+    icon: 'menu', 
+    active: true },
+
+    {
+      title: 'Calendário',
+      url: '/folder/Calendario',
+      icon: 'calendario',
+      active: false,
+    },
+    
+    {
+      title: 'Notificações',
+      url: '/folder/Notificacoes',
+      icon: 'notificacao',
+      active: false,
+    },
+  ];
+
+  constructor() {
+    this.addAllIcons();
+  }
+
+  addAllIcons() {
+    addIcons({
+      filho: person,
+      calendario: calendar,
+      notificacao: notifications,
+      conversas: chatbubbles,
+      status: happy,
+      menu: menu,
+      menuOutline: menuOutline,
+      menuSharp: menuSharp,
+    });
+  }
 }
