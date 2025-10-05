b8fa6928985308e5453ae6d7abfa0f6767dd10bf
DanielSln <12danielsolano@gmail.com>
2025-10-05 00:23:47 -0300

    create: page comunicados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -38,6 +38,10 @@ export const routes: Routes = [
   {
     path: 'status',
     loadComponent: () => import('./pages/status/status.page').then( m => m.StatusPage)
+  },  {
+    path: 'comunicados',
+    loadComponent: () => import('./pages/comunicados/comunicados.page').then( m => m.ComunicadosPage)
   },
+
 
 ];
