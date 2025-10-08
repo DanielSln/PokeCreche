6d34e449d84f1ed2ce2617128555f2fb1af6e9a4
DanielSln <12danielsolano@gmail.com>
2025-10-08 16:49:29 -0300

    style: style page termos and ts

--- src/app/pages/termos/termos.page.ts
+++ src/app/pages/termos/termos.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonButton,
+} from '@ionic/angular/standalone';
+
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-termos',
-  templateUrl: './termos.page.html',
-  styleUrls: ['./termos.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  templateUrl: 'termos.page.html',
+  styleUrls: ['termos.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    IonButton,
+  ],
 })
-export class TermosPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TermosPage {
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  aceitarTermos() {
+    this.router.navigateByUrl('/cadastro-aluno');
   }
-
 }
