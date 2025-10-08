63921fa958f96c051dfbbc4ba979eccc6c424d62
DanielSln <12danielsolano@gmail.com>
2025-10-08 16:51:41 -0300

    fix: ts menu-docente

--- src/app/pages/menu-docente/menu-docente.page.ts
+++ src/app/pages/menu-docente/menu-docente.page.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonMenuButton,
+  IonButtons,
+} from '@ionic/angular/standalone';
+
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-docente',
   templateUrl: './menu-docente.page.html',
   styleUrls: ['./menu-docente.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButton,
+    IonMenuButton,
+    IonButtons,
+  ],
 })
 export class MenuDocentePage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  irParaDocente() {
+    this.router.navigateByUrl('/docente');
   }
-
 }
