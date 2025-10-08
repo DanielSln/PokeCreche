01ae5db7badd39ffc2c42cf730023b9fdc8ba88a
DanielSln <12danielsolano@gmail.com>
2025-10-08 17:03:06 -0300

    fix: redirect initial screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'termos',
+    redirectTo: 'login-aluno',
     pathMatch: 'full',
   },
 
