4ccac81524bc3e6bffb41ebb47f78351a612349a
Anthony3043 <am1664827@gmail.com>
2025-10-10 14:46:54 -0300

    fix  : coloquei ion-back-buttons e mudei o menu de lado

--- src/app/pages/turmas/turmas.page.ts
+++ src/app/pages/turmas/turmas.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonBackButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './turmas.page.html',
   styleUrls: ['./turmas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonButtons]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonButtons, IonBackButton]
 })
 export class TurmasPage implements OnInit {
 
