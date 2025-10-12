6c543cac9847841bc740850ce7d7a05f1593f2ad
Anthony3043 <am1662847@gmai.com>
2025-10-12 20:21:01 -0300

    fix ; fiz uma tela em que ao clicar no aluno te entregará um formulario para dizer como o aluno esta (alimentação, comportamento e presença)

--- src/app/pages/turmas/turmas.page.ts
+++ src/app/pages/turmas/turmas.page.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonBackButton, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { chevronBack } from 'ionicons/icons';
 
 @Component({
   selector: 'app-turmas',
   templateUrl: './turmas.page.html',
   styleUrls: ['./turmas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonButtons, IonBackButton, IonButton, IonIcon]
 })
 export class TurmasPage implements OnInit {
 
   query: string = '';
   showEditModal: boolean = false;
   novoNomeTurma: string = '';
+  showAlunoModal: boolean = false;
+  alunoSelecionado: any = null;
 
   turmas = Array.from({length: 12}).map((_,i) => ({ id: (i*37 + 32).toString().padStart(4,'0'), selected: false }));
 
   selectedTurma: any = null;
   studentsByTurma: Record<string, any[]> = {};
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    addIcons({ chevronBack });
+  }
 
   ngOnInit() {
     this.carregarTurmas();
@@ -66,8 +72,26 @@ export class TurmasPage implements OnInit {
     student.falta = !student.falta;
   }
 
-  openStatus(student:any) {
-    this.router.navigateByUrl('/status');
+  abrirRegistroAluno(student: any) {
+    this.alunoSelecionado = { ...student };
+    this.showAlunoModal = true;
+  }
+
+  fecharAlunoModal() {
+    this.showAlunoModal = false;
+    this.alunoSelecionado = null;
+  }
+
+  salvarRegistroAluno() {
+    if (this.alunoSelecionado) {
+      const aluno = this.studentsByTurma[this.selectedTurma.id].find(s => s.id === this.alunoSelecionado.id);
+      if (aluno) {
+        aluno.alimentacao = this.alunoSelecionado.alimentacao;
+        aluno.comportamento = this.alunoSelecionado.comportamento;
+        aluno.presenca = this.alunoSelecionado.presenca;
+      }
+    }
+    this.fecharAlunoModal();
   }
 
   editarNomeTurma() {
